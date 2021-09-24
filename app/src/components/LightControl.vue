23ff8f694a5affacef575eaccaf696f9ed59ed66
MarkusKiesel93 <markus.kiesel@tuta.io>
2021-09-24 13:45:33 +0200

    remove hello world add basic controls

---
+++ app/src/components/LightControl.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols=12>
+        <v-card>
+          <v-card-title>
+            Smart Light Controls
+          </v-card-title>
+          <v-card-actions>
+            <v-row>
+              <v-col cols=12>
+                <v-text-field>
+                  Input
+                </v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols=12>
+                <v-btn
+                  color="green"
+                >
+                  <v-icon>
+                    mdi-check
+                  </v-icon>
+                  Light On
+                </v-btn>
+                <v-btn
+                  color="red"
+                >
+                  <v-icon>
+                    mdi-check
+                  </v-icon>
+                  Light Off
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols=12>
+        <v-card>
+          <v-card-title>
+            Ambient Light Controls
+          </v-card-title>
+          <v-card-actions>
+            <v-btn
+              color="green"
+            >
+              <v-icon>
+                mdi-check
+              </v-icon>
+              Light On
+            </v-btn>
+            <v-btn
+              color="red"
+            >
+              <v-icon>
+                mdi-check
+              </v-icon>
+              Light Off
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'LightControl'
+}
+
+</script>
